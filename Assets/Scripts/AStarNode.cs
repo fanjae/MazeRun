0179b7a04f6bc01779a6d1759d17190e305e7cbf
FanJae <hjjang45@gmail.com>
2026-07-21 22:49:24 +0900

    feat: A* 탐색용 노드 데이터 모델 추가
    - AStarNode 클래스 추가
    - 노드 좌표 및 이동 가능 여부 관리
    - G, H, F Cost와 Parent 정보 추가
    - 탐색 데이터 초기화 기능 구현

---
+++ Assets/Scripts/AStarNode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class AStarNode
+{
+    // 격자 좌표
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    // 이동 가능한 칸인지 여부
+    public bool IsWalkable { get; set; }
+
+    // 시작점과 목적지 표시용
+    public bool IsStart { get; set; }
+    public bool IsGoal { get; set; }
+
+    // A* 비용
+    // G : 시작 노드부터 현재 노드 까지 비용
+    // H : 목표 노드까지 예상 비용
+    public int GCost { get; set; }
+    public int HCost { get; set; }
+
+    public int FCost
+    {
+        get
+        {
+            if (GCost == int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+
+            return GCost + HCost;
+        }
+    }
+
+    // 경로를 역추적하기 위한 이전 노드
+    public AStarNode Parent { get; set; }
+
+    // 해당 노드의 화면 오브젝트
+    public NodeView View { get; private set; }
+
+    public AStarNode(int x, int y, NodeView view)
+    {
+        X = x;
+        Y = y;
+        View = view;
+
+        IsWalkable = true;
+        IsStart = false;
+        IsGoal = false;
+
+        ResetSearchData();
+    }
+
+    public void ResetSearchData()
+    {
+        GCost = int.MaxValue;
+        HCost = 0;
+        Parent = null;
+    }
+}
